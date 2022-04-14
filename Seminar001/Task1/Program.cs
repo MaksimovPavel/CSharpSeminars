2560db195a5e534ab0cc3e3dc4ae6a557ba58a38
Pavel <89213158645p@gmail.com>
2022-04-14 11:39:59 +0300

    Task 1 completed

---
+++ Seminar001/Task1/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Write("Input integer number 1: ");
+int numberA = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Input integer number 2: ");
+int numberB = Convert.ToInt32(Console.ReadLine());
+
+if(numberA > numberB)
+{
+    Console.WriteLine("Max = " + numberA);
+}
+else
+{
+    Console.WriteLine("Max = " + numberB);
+}

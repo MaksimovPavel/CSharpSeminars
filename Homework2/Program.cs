a256cece8cb33aa8e5eadbbea8a10e64aac0255a
Pavel <89213158645p@gmail.com>
2022-04-18 17:42:08 +0300

    Task 1 completed

--- Homework2/Program.cs
+++ Homework2/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+/*
+void ShowNumber (int num)
+{
+    int firstNum = num / 10;
+    int secondNum = firstNum % 10;
+
+    Console.Write(num + " -> ");
+    Console.Write(secondNum);
+    
+}
+int num = new Random().Next(100, 999);
+ShowNumber(num);
+*/
+
+
+

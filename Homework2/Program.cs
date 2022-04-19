3f35f7fe1e3c52aeebcf7fef7ec81c9c22636b02
Pavel <89213158645p@gmail.com>
2022-04-19 14:41:29 +0300

    Task 2 completed

--- Homework2/Program.cs
+++ Homework2/Program.cs
@@ -11,7 +11,64 @@ void ShowNumber (int num)
 }
 int num = new Random().Next(100, 999);
 ShowNumber(num);
+
+
+//Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+
+void ShowNumber (int num)
+{
+    if (num >= 100 && num <1000)
+    {
+    int n = num % 10;    
+    Console.Write(num + " -> ");
+    Console.Write(n);
+    }
+    if (num >= 1000 && num <= 9999)
+    {
+    int n = num / 10 % 10;    
+    Console.Write(num + " -> ");
+    Console.Write(n);
+    }
+    if (num >= 10000 && num <= 99999)
+    {
+    int n = num / 10 % 10;    
+    Console.Write(num + " -> ");
+    Console.Write(n);
+    }
+    else
+    {
+        Console.Write("No third number");
+    }
+}
+Console.Write("Input number: ");
+int num = Convert.ToInt32(Console.ReadLine());
+ShowNumber(num);
+
 */
 
-
+Console.Write("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+int amountOfNumber = 0; 
+int counter = num;
+while (counter>0)
+    {
+    amountOfNumber++;
+    counter = counter / 10;
+    }
+counter = 1;
+int result = num;
+    if (amountOfNumber > 2)
+    {
+        while (counter < amountOfNumber - 2 )
+            {
+            result = result /10;
+            counter++;
+         }
+        Console.Write("Third number is: " + result %10);
+    }   
+    else
+    {
+        Console.Write("No third number");
+    }
+    
 

f0cc898b69bd0200a02ac6cdbc098a3ae3e84fc7
Pavel <89213158645p@gmail.com>
2022-04-27 15:50:53 +0300

    Task 1 completed

---
+++ Homework5/Program.cs
@@ -0,0 +1,23 @@
+﻿//Задача 1: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+int myfunc(int number, int degree)
+{
+    int result = number;
+    for(int i = 1; i < degree; i++)
+    {
+        result *= number;
+    }
+    return result;
+}
+Console.Write("Input number A: ");
+int numA = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input number B: ");
+int numB = Convert.ToInt32(Console.ReadLine());
+if (numB < 0)
+{
+    Console.Write("Input natural number!");
+    //numB = -numB; либо такой вариант 
+}
+else
+Console.Write( numA + "," + numB + " -> "  + myfunc(numA,numB));
+
+

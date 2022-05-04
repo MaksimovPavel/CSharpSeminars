7ab7c623c758dcbc0c5bbb4b051cecb09992d570
Pavel <89213158645p@gmail.com>
2022-05-04 17:07:17 +0300

    Homework 6 completed

--- Homework6/Program.cs
+++ Homework6/Program.cs
@@ -42,41 +42,93 @@ num /= 2;
 }
 Console.Write("Number in 2 system is:" +s);
 Console.ReadKey();
-*/
-Console.Write("Input number: ");
+
+
+Console.Write("Input number to convert - ");
 int number = Convert.ToInt32(Console.ReadLine());
 
-    int[] array = new int [10];
-    int result = 0;
-    int num = number;
-    while (num> 0)
+int[] array = new int [10];
+int result = 0;
+int num = number;
+while(num > 0)
+{
+    for(int i = 0; i < 10; i++)
     {
-        for(int i = 0; i < 10; i++)
-        {
-            result = num % 2;
-            num /= 2;
-            array[i] = result; 
-        }     
+        result = num % 2;
+        num /= 2;
+        array[i] = result;
     }
+} 
+
 void ShowArray(int[] array)
 {
-    for (int i = 0; i < 10; i++)
-    {
-        Console.Write(array[i] + "");
-    }
+    for(int i = 0; i < array.Length; i++)
+        Console.Write(array[i] + " ");
+    Console.WriteLine();
 }
-ShowArray(array);
-
-int[] NewArray(int[] array)
+int[] ReverseArray(int[] array) // 1 -4 3 -2 6 -1 5 3 8 -5
 {
     int newSize = array.Length;
     int[] newArray = new int[newSize];
 
-    for (int i = 0, j = array.Length; i < newSize; i++, j--)
+    for (int i = 0, j = array.Length - 1; i < newSize; i++,j--)
     {
         newArray[i] = array[j];
     }
-    return NewArray;
+    return newArray;
 }
-int[] newArray = new int [10];
-ShowArray(newArray);
+
+int[] newArr = ReverseArray(array);
+ShowArray(array);
+ReverseArray(array);
+Console.Write("result is: " );
+ShowArray(newArr);
+*/
+
+//Напишите программу, которая будет преобразовывать десятичное число в двоичное.
+string Calculator(int num, string binary)
+{
+    if(num<=1)
+    {
+        string d = Convert.ToString(num);
+        binary = d+binary;
+        return binary;
+    }
+    return Calculator(num / 2, Convert.ToString(num - 2 * (num/2)) + binary);
+}
+Console.WriteLine("Input number: ");
+int number = Convert.ToInt32(Console.ReadLine());
+string bin = "";
+Console.WriteLine("Number in binary system: " + Calculator(number,bin));
+
+/*
+//Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+
+Console.WriteLine("Input b1: ");
+int b1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Input k1: ");
+int k1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Input b2: ");
+int b2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Input k2: ");
+int k2 = Convert.ToInt32(Console.ReadLine());
+int h1 = 1;
+int h2 = 1;
+    //Представил уравнения в другом виде:
+    //k1*x - h1*y1 + b1 = 0;
+    //k2*x - h2*y2 + b2 = 0;
+    // double pCrossX = ((h1 * b2) - (h2 * b1)) / ((k1 * h2) - (k2 * h1)); 
+    // double pCrossY = (k2 * b1 - k1 * b2) / (k1 * h2 - k2 * h1);
+    // инфу по нахождению точек пересечения взял с сайта https://www.interestprograms.ru/source-codes-tochka-peresecheniya-dvuh-pryamyh-na-ploskosti
+double num = (h1 * b2) - (h2 * b1);
+double num1 = (k1 * h2) - (k2 * h1);
+double pCrossX = (num / num1);
+
+double number = (k2 * b1) - (k1 * b2);
+double number1 = (k1 * h2) - (k2 * h1);
+double pCrossY = (number / number1);
+
+Console.Write("Кординаты точки перечесечения на оси X: " + pCrossX);
+Console.WriteLine();
+Console.Write("Кординаты точки перечесечения оси Y: " + pCrossY);
+*/
